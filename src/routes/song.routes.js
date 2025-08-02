888d2826c917670c7653fc1254682ce2af6bd921
Devidas Arse <devidasarse25@gmail.com>
2025-08-02 13:38:27 +0530

    update storage.service.js

--- src/routes/song.routes.js
+++ src/routes/song.routes.js
@@ -3,18 +3,42 @@ const multer = require("multer");
 const uploadFile= require("../service/storage.service");
 const router = express.Router();
 
+const songMOdel = require("../models/song.model");// Import the song model 
+
 const upload = multer({ storage: multer.memoryStorage() });
 // src/routes/song.routes.js
 router.post("/songs", upload.single("audio"), async (req, res) => {
   console.log(req.body);
   console.log(req.file);
   const fileData = await uploadFile(req.file);
-    console.log(fileData);
+    // console.log(fileData);
+    const song = await songMOdel.create({
+      title: req.body.title,
+      artist: req.body.artist,
+      audio: fileData.url, // Use the URL from ImageKit
+      mood: req.body.mood, // Assuming mood is also sent in the request body
+    })
+
+
   res.status(201).json({
     message: "All songs fetched successfully",
     songs: req.body, 
   
   });
+});
+
+
+router.get("/songs", async (req, res) => {
+   const {mood} = req.query; // mood = sad
+
+   const songs = await songMOdel.find({
+    mood: mood// mood body me nhi ayenga
+   })
+
+   res.status(200).json({
+    message: " Song feached success",
+    songs
+   })
 });
 
 // (Optional) Add more routes like POST, PUT, DELETE here
--- src/service/storage.service.js
+++ src/service/storage.service.js
@@ -1,5 +1,7 @@
 var Imagekit = require("imagekit");
 
+var mongoose = require("mongoose")
+
 var imagekit = new Imagekit({
     publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
@@ -11,7 +13,8 @@ function uploadFile(file) {
     return new Promise((resolve, reject) => {
      imagekit.upload({
         file: file.buffer, // the file buffer
-        fileName: "hello-cohort"
+        fileName:(new mongoose.Types.ObjectId()).toString(), // file name to be saved in ImageKit
+        folder: "cohort-audio", // optional, can be used to specify a folder in ImageKit
     },(error, result) => {
         if (error) {
             reject(error);
